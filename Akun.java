63c2f4f9f8682b4c35f8faa5c54ee27d11f880cf
Ikhsans4 <88651883+Ikhsans4@users.noreply.github.com>
2021-11-21 15:22:30 +0700

    add new method

--- Akun.java
+++ Akun.java
@@ -1,9 +1,8 @@
-
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+
 
 /**
  * Class akun untuk mengelola akun admin dan pendaftar sistem PPDB
@@ -16,17 +15,21 @@ import java.util.StringTokenizer;
 public class Akun {
     private String username;
     private String password;
+    private String namaLengkap;
+    private String jenisKelamin;
 
     /**
      * Method constructor untuk class Akun.
-     * 
      * @param username
      * @param password
+     * @param namaLengkap
+     * @param jenisKelamin
      */
-    public Akun(String username, String password) {
+    public Akun(String username, String password, String namaLengkap, String jenisKelamin) {
         this.username = username;
         this.password = password;
-
+        this.namaLengkap = namaLengkap;
+        this.jenisKelamin = jenisKelamin;
     }
 
     /**
@@ -46,12 +49,29 @@ public class Akun {
     public String getPassword() {
         return this.password;
     }
+
+    /**
+     * Method getter nama lengkap user
+     * @return namaLengkap
+     */
+    public String getNamaLengkap() {
+        return this.namaLengkap;
+    }
+    /**
+     * Method getter jenis kelamin user
+     * @return jenisKelamin
+     */
+    public String getJenisKelamin() {
+        return this.jenisKelamin;
+    }
+
+
+
     /**
      * Method untuk mengecek username dan password user
      * @return login
      * @throws IOException
      */
-
     public boolean cekUsernameAndPassword() throws IOException{
         // membuka file
         boolean login;
@@ -62,7 +82,7 @@ public class Akun {
         bufferInput = new BufferedReader(inputFile);
 
         String data;
-
+        
         // mengecek inputan user dengan data didalam database
         while (true) {
             data = bufferInput.readLine();
